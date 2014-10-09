c04527a7e39409734a672441414cf2a10993f34e
seles <seles@yandex-team.ru>
2014-10-09 18:26:32 +0400

    Мелкие правки

--- src/rules/copy.js
+++ src/rules/copy.js
@@ -1,3 +1,6 @@
 Typograf.rule('copy', 10, function(text) {
-    return text.replace(/\(r\)/gi, '®').replace(/\((c|с)\)/gi, '©').replace(/\(tm\)/gi, '™').replace(/\+\/\-/gi, '±');
+    return text.replace(/\(r\)/gi, '®')
+        .replace(/\((c|с)\)/gi, '©')
+        .replace(/\(tm\)/gi, '™')
+        .replace(/\+\/\-/gi, '±');
 });
