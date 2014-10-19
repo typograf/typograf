6c8cd49e4d9e42c233eda206b7793f5cd8b1125c
Denis Seleznev <hcodes@yandex.ru>
2014-10-19 14:15:49 +0400

    Удалять пробелы в конце строк #8

--- typograf.js
+++ typograf.js
@@ -2456,6 +2456,33 @@ Typograf.rule({
 });
 
 Typograf.rule({
+    title: 'Удаление пробелов в конце строк',
+    name: 'space:del_trailing_blanks',
+    sortIndex: 505,
+    func: function(text) {
+        return text.replace(/\s+\n/g, '\n');
+    }
+});
+
+Typograf.rule({
+    title: 'Удаление пробелов в конце строк',
+    name: 'space:del_trailing_blanks',
+    sortIndex: 505,
+    func: function(text) {
+        return text.replace(/\s+\n/g, '\n');
+    }
+});
+
+Typograf.rule({
+    title: 'Удаление пробелов в конце строк',
+    name: 'space:del_trailing_blanks',
+    sortIndex: 505,
+    func: function(text) {
+        return text.replace(/\s+\n/g, '\n');
+    }
+});
+
+Typograf.rule({
     title: 'Замена табов на пробелы',
     name: 'space:replace_tab',
     sortIndex: 510,
