7d5d01447fb0fe9c87cc8fd0122a694855ccad28
Denis Seleznev <hcodes@yandex.ru>
2014-10-20 22:02:56 +0400

    Добавить правило или метод для удаления HTML-тегов #11

---
+++ src/rules/strip_tags.spec.js
@@ -0,0 +1,5 @@
+tests.push(['strip_tags', [
+    ['123123 12<br/>12312 312 3<p>asdlalsdpa</p>', '123123 1212312 312 3asdlalsdpa'],
+    ['<p', '<p'],
+    ['<p align="center">Hello</p>', 'Hello']
+]]);
