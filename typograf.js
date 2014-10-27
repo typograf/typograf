fc4a133c5bb9c388c2aea516d13caff009976976
Denis Seleznev <hcodes@yandex.ru>
2014-10-27 23:26:43 +0300

    Фикс тестов для евро

--- typograf.js
+++ typograf.js
@@ -770,7 +770,7 @@ Typograf.rule({
     sortIndex: 1140,
     func: function(text) {
         var re1 = new RegExp('(^|[\\D]{2,})\\$ ?([\\d.,]+)', 'g'),
-            re2 = new RegExp('(^|[\\D])([\\d.m]+) ?\\$'),
+            re2 = new RegExp('(^|[\\D])([\\d.,]+) ?\\$'),
             rep = '$1$2\u00A0\$';
 
         return text
