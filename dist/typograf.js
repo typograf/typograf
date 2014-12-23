50019f8201e90102ea4356322409d441525964f3
Denis Seleznev <hcodes@yandex.ru>
2014-12-24 00:48:06 +0300

    Дополнено правило ru/nbsp/ooo

--- dist/typograf.js
+++ dist/typograf.js
@@ -1244,11 +1244,11 @@ Typograf.rule({
 });
 
 Typograf.rule({
-    title: 'Неразрывный пробел после OOO или ОАО',
+    title: 'Неразрывный пробел после OOO, ОАО, ЗАО, НИИ и ПБОЮЛ',
     name: 'ru/nbsp/ooo',
     sortIndex: 1100,
     func: function(text) {
-        return text.replace(/(ООО|ОАО) /g, '$1\u00A0');
+        return text.replace(/(^|[^a-яёA-ЯЁ])(ООО|ОАО|ЗАО|НИИ|ПБОЮЛ) /g, '$1$2\u00A0');
     }
 });
 
