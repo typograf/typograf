cf136c67f30d4e006faf253637f90829baeedc0b
Denis Seleznev <hcodes@yandex.ru>
2014-12-27 14:37:13 +0300

    Правила должны быть в группа�

--- src/rules/common/other/repeatWord.js
+++ src/rules/common/other/repeatWord.js
@@ -1,6 +1,6 @@
 Typograf.rule({
     title: 'Удаление повтора слова',
-    name: 'common/repeatWord',
+    name: 'common/other/repeatWord',
     sortIndex: 1200,
     func: function(text) {
         return text.replace(/([a-zа-яё\u0301]+) \1([;:,.?! \n])/gi, '$1$2');
