a38246cff2b32456713b382c78d85ae5fcdcdf26
Denis Seleznev <hcodes@yandex.ru>
2015-01-14 21:59:49 +0300

    Extra space between close quote and ? or ! #75

--- src/rules/common/space/afterPunctuation.js
+++ src/rules/common/space/afterPunctuation.js
@@ -3,8 +3,9 @@ Typograf.rule({
     name: 'common/space/afterPunctuation', 
     sortIndex: 560, 
     func: function(text) {
+        var re = new RegExp('(!|;|\\?)([^ \uDBFF\n\t!;?' + this.data['common/quotes'] + '[])', 'g');
         return text
-            .replace(/(!|;|\?)([^ \uDBFF\n\t!;?[])/g, '$1 $2')
+            .replace(re, '$1 $2')
             .replace(/(\D)(,|:)([^ \uDBFF\n\t,.?:])/g, '$1$2 $3');
     }
 });
