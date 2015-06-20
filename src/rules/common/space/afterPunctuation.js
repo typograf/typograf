0f48cf47b793764a2562b38cd24d52bdf475961a
Fedor Borshev <f@f213.in>
2015-06-20 16:35:33 +0300

    Fixed incorrect space between exclamation mark and closing bracket

--- src/rules/common/space/afterPunctuation.js
+++ src/rules/common/space/afterPunctuation.js
@@ -2,7 +2,7 @@ Typograf.rule({
     name: 'common/space/afterPunctuation', 
     sortIndex: 560, 
     func: function(text) {
-        var re = new RegExp('(!|;|\\?)([^ \uDBFF\n\t!;?' + this.data('common/quot') + '[])', 'g');
+        var re = new RegExp('(!|;|\\?)([^ \)\uDBFF\n\t!;?' + this.data('common/quot') + '[])', 'g');
         return text
             .replace(re, '$1 $2')
             .replace(/(\D)(,|:)([^ \/\uDBFF\n\t,.?:])/g, '$1$2 $3');
