cd4e1dab11643a6e58d502879fbfc53cb6da25a3
Denis Seleznev <hcodes@yandex.ru>
2015-08-13 00:05:20 +0300

    Собирать файлы для тестирования в папке build/

--- test/test.smoke.common.js
+++ test/test.smoke.common.js
@@ -1,5 +1,5 @@
 var assert = require('chai').assert,
-    Typograf = require('../dist/typograf'),
+    Typograf = require('../build/typograf'),
     t = new Typograf(),
     commonTests = [
         ['  start  <script>  var a = 10;  \n\n\n</script>  end  ',
