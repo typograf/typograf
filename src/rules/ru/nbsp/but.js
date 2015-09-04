6de945c83169ff739bf5cfdbea12e1d52dd908f5
Denis Seleznev <hcodes@yandex.ru>
2015-09-05 00:55:37 +0300

    Ошибка в расстановке запятой перед а и но #119

--- src/rules/ru/nbsp/but.js
+++
@@ -1,8 +0,0 @@
-Typograf.rule({
-    name: 'ru/nbsp/but',
-    index: 1110,
-    handler: function(text) {
-        var re = new RegExp(',?( |\u00A0|\n)(а|но)( |\u00A0|\n)', 'g');
-        return text.replace(re, ',$1$2$3');
-    }
-});
