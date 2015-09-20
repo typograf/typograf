5abf566fef7bab5f12fe9cd4e4b666f620e970bf
Denis Seleznev <hcodes@yandex.ru>
2015-09-20 23:02:48 +0300

    Добавить правило для P. S. #122

---
+++ src/rules/ru/nbsp/ps.js
@@ -0,0 +1,11 @@
+/*jshint maxlen:1000 */
+Typograf.rule({
+    name: 'ru/nbsp/ps',
+    index: 565,
+    handler: function(text) {
+        var re = new RegExp('(^|\\s|' + Typograf._privateLabel + ')[pз]\\.[ \u00A0]?([pз]\\.[ \u00A0]?)?[sы]\\.:? ', 'gim');
+        return text.replace(re, function($0, $1, $2) {
+            return $1 + ($2 ? 'P.\u00A0P.\u00A0S. ' : 'P.\u00A0S. ');
+        });
+    }
+});
