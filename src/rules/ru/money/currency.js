3c87d935d8ab844779992eb1fb6fc15cd925a697
Denis Seleznev <hcodes@yandex.ru>
2015-11-08 15:24:38 +0300

    Объединение правил по символам валют

---
+++ src/rules/ru/money/currency.js
@@ -0,0 +1,14 @@
+Typograf.rule({
+    name: 'ru/money/currency',
+    handler: function(text) {
+        var currency = '([$€¥Ұ£₤])',
+            re1 = new RegExp('(^|[\\D]{2,})' + currency + ' ?([\\d.,]+([ \u00A0\u2009\u202F]\\d{3})*)', 'g'),
+            re2 = new RegExp('(^|[\\D])([\\d.,]+) ?' + currency, 'g'),
+            newSubstr1 = '$1$3\u00A0$2',
+            newSubstr2 = '$1$2\u00A0$3';
+
+        return text
+            .replace(re1, newSubstr1)
+            .replace(re2, newSubstr2);
+    }
+});
