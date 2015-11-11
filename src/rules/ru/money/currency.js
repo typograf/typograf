89039c03f08cbb54f552fe300a3469b10ce3852b
Denis Seleznev <hcodes@yandex.ru>
2015-11-11 08:23:41 +0300

    Добавил знак рубля

--- src/rules/ru/money/currency.js
+++ src/rules/ru/money/currency.js
@@ -1,7 +1,7 @@
 Typograf.rule({
     name: 'ru/money/currency',
     handler: function(text) {
-        var currency = '([$€¥Ұ£₤])',
+        var currency = '([$€¥Ұ£₤₽])',
             re1 = new RegExp('(^|[\\D]{2,})' + currency + ' ?([\\d.,]+([ \u00A0\u2009\u202F]\\d{3})*)', 'g'),
             re2 = new RegExp('(^|[\\D])([\\d.,]+) ?' + currency, 'g'),
             newSubstr1 = '$1$3\u00A0$2',
