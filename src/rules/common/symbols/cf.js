6739c0b230750ce95c609ca2f38755512dcc1d9c
Denis Seleznev <hcodes@yandex.ru>
2015-11-18 22:57:45 +0300

    Доработки правила common/symbols/cf для ссылок

--- src/rules/common/symbols/cf.js
+++ src/rules/common/symbols/cf.js
@@ -1,8 +1,8 @@
 Typograf.rule({
     name: 'common/symbols/cf',
     handler: function(text) {
-        var re = new RegExp('(\\d+)( |\u00A0)?(C|F)([\\W \\.,:!\\?"\\]\\)]|$)', 'g');
+        var re = new RegExp('(^|[^%])(\\d+)( |\u00A0)?(C|F)([\\W \\.,:!\\?"\\]\\)]|$)', 'g');
 
-        return text.replace(re, '$1' + '\u2009' + '°$3$4');
+        return text.replace(re, '$1$2' + '\u2009' + '°$4$5');
     }
 });
--- src/rules/common/symbols/cf.spec.js
+++ src/rules/common/symbols/cf.spec.js
@@ -1,7 +1,14 @@
 tests.push(['common/symbols/cf', [
     [' 200 C', ' 200 °C'],
+    ['200 C', '200 °C'],
+    ['200 C\n 300 F', '200 °C\n 300 °F'],
     [' 200 C.', ' 200 °C.'],
     [' 20d C', ' 20d C'],
     [' 20 C1', ' 20 C1'],
-    [' 200 F', ' 200 °F']
+    [' 200 F', ' 200 °F'],
+    ['200 F', '200 °F'],
+    [
+        'https://ru.wikipedia.org/wiki/%D0%A4%D0%B0%D0%BC%D0%B8%D0%BB%D1%8C',
+        'https://ru.wikipedia.org/wiki/%D0%A4%D0%B0%D0%BC%D0%B8%D0%BB%D1%8C'
+    ]
 ]]);
