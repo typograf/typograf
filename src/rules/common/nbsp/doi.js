182fb71419b2957da633dd4873818a83e74cc8ad
Denis Seleznev <hcodes@yandex.ru>
2016-02-03 00:55:10 +0300

    Удалено правило common/nbsp/doi

--- src/rules/common/nbsp/doi.js
+++
@@ -1,7 +0,0 @@
-Typograf.rule({
-    name: 'common/nbsp/doi',
-    handler: function(text) {
-        var re = new RegExp('(^|[\\s([' + Typograf._privateLabel + '])DOI (?=\\d)', 'g');
-        return text.replace(re, '$1DOI\u00A0');
-    }
-});
