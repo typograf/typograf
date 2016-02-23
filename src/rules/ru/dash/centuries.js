de4d5e44a6e7015105a98d6c4fa385696466f5ba
Denis Seleznev <hcodes@yandex.ru>
2016-02-23 22:45:46 +0300

    Мелкий рефакторинг правил

--- src/rules/ru/dash/centuries.js
+++ src/rules/ru/dash/centuries.js
@@ -1,10 +1,10 @@
 Typograf.rule({
     name: 'ru/dash/centuries',
-    handler: function(text) {
+    handler: function(text, settings) {
         var dashes = '(' + this.data('common/dash') + ')',
             re = new RegExp('(X|I|V)[ |\u00A0]?' + dashes + '[ |\u00A0]?(X|I|V)', 'g');
 
-        return text.replace(re, '$1' + this.setting('ru/dash/centuries', 'dash') + '$3');
+        return text.replace(re, '$1' + settings.dash + '$3');
     },
     settings: {
         dash: '\u2014' // &mdash;
--- src/rules/ru/dash/years.js
+++ src/rules/ru/dash/years.js
@@ -1,14 +1,13 @@
 Typograf.rule({
     name: 'ru/dash/years',
-    handler: function(text) {
-        var dash = this.setting('ru/dash/years', 'dash'),
-            dashes = this.data('common/dash'),
+    handler: function(text, settings) {
+        var dashes = this.data('common/dash'),
             re = new RegExp('(\\D|^)(\\d{4})[ \u00A0]?(' +
                 dashes + ')[ \u00A0]?(\\d{4})(?=[ \u00A0]?г)', 'g');
 
         return text.replace(re, function($0, $1, $2, $3, $4) {
             if(parseInt($2, 10) < parseInt($4, 10)) {
-                return $1 + $2 + dash + $4;
+                return $1 + $2 + settings.dash + $4;
             }
 
             return $0;
