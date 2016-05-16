029428c54387d9a9912911a7d4735b837d4a2a31
Denis Seleznev <hcodes@yandex.ru>
2016-05-16 23:41:37 +0300

    Исправил ошибку, снижающую скорость типографа

--- src/rules/ru/money/currency.js
+++ src/rules/ru/money/currency.js
@@ -2,7 +2,7 @@ Typograf.rule({
     name: 'ru/money/currency',
     handler: function(text) {
         var currency = '([$€¥Ұ£₤₽])',
-            re1 = new RegExp('(^|[\\D]{2,})' + currency + ' ?([\\d.,]+([ \u00A0\u2009\u202F]\\d{3})*)', 'g'),
+            re1 = new RegExp('(^|[\\D]{2})' + currency + ' ?([\\d.,]+([ \u00A0\u2009\u202F]\\d{3})*)', 'g'),
             re2 = new RegExp('(^|[\\D])([\\d.,]+) ?' + currency, 'g'),
             newSubstr1 = '$1$3\u00A0$2',
             newSubstr2 = '$1$2\u00A0$3';
