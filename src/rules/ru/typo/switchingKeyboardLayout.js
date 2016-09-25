37590535003a38ee0c8a429549d5c24ac0ea621b
Denis Seleznev <hcodes@yandex.ru>
2016-09-25 21:24:08 +0300

    Правило для замены латинских букв на русские
    Опечатки, возникающие при переключении клавиатурной раскладки.

---
+++ src/rules/ru/typo/switchingKeyboardLayout.js
@@ -0,0 +1,42 @@
+(function() {
+
+var replacements = {
+    A: 'А', // Latin: Russian
+    a: 'а',
+    B: 'В',
+    E: 'Е',
+    e: 'е',
+    K: 'К',
+    M: 'М',
+    H: 'Н',
+    O: 'О',
+    o: 'о',
+    P: 'р',
+    p: 'Р',
+    C: 'С',
+    c: 'с',
+    T: 'Т',
+    y: 'у',
+    X: 'Х',
+    x: 'х'
+};
+
+var keys = Object.keys(replacements).join('');
+
+Typograf.rule({
+    name: 'ru/typo/switchingKeyboardLayout',
+    handler: function(text) {
+        var re = new RegExp('([' + keys + ']{1,3})(?=[А-ЯЁа-яё]+?)', 'g');
+
+        return text.replace(re, function(str, $1) {
+            var result = '';
+            for (var i = 0; i < $1.length; i++) {
+                result += replacements[$1[i]];
+            }
+
+            return result;
+        });
+    }
+});
+
+})();
---
+++ src/rules/ru/typo/switchingKeyboardLayout.spec.js
@@ -0,0 +1,18 @@
+tests.push(['ru/typo/switchingKeyboardLayout', [
+    [
+        'Cпокойствие', // C - latin letter
+        'Спокойствие' // C - russian letter
+    ],
+    [
+        'Ccылка', // Cc - latin letter
+        'Ссылка' // Сс - russian letter
+    ],
+    [
+        'Ccылка, ccылка и ещё ccылка',
+        'Ссылка, ссылка и ещё ссылка'
+    ],
+    [
+      'Первая ccылка\nВторая ccылка\n...\nCедьмая ccылка',
+      'Первая ссылка\nВторая ссылка\n...\nСедьмая ссылка'
+    ]
+]]);
