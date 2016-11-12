ce6ebb6d17fb7d002985ae5e40da80fb7399a80d
Denis Seleznev <hcodes@yandex.ru>
2016-11-12 11:42:09 +0300

    common/html/pbr -> common/html/p

--- src/rules/common/html/nbr.spec.js
+++ src/rules/common/html/nbr.spec.js
@@ -1,4 +1,6 @@
 tests.push(['common/html/nbr', [
     ['a\nb\nc', 'a<br/>\nb<br/>\nc'],
-    ['a<br/>\nb\nc', 'a<br/>\nb\nc']
+    ['a<br/>\nb\nc', 'a<br/>\nb<br/>\nc'],
+    ['a\n\nb\nc', 'a\n\nb<br/>\nc'],
+    ['\na\n\nb\nc\n\nd\ne\n', '\na\n\nb<br/>\nc\n\nd<br/>\ne\n']
 ]]);
--- src/rules/common/html/pbr.spec.js
+++
@@ -1,6 +0,0 @@
-tests.push(['common/html/pbr', [
-    ['a\n\nb\nc\n\nd', '<p>a</p>\n<p>b<br/>\nc</p>\n<p>d</p>'],
-    ['a', '<p>a</p>'],
-    ['a\n\n\nb<br/>', 'a\n\n\nb<br/>'],
-    ['<p>a</p>\n\n\n<p>b</p>', '<p>a</p>\n\n\n<p>b</p>']
-]]);
