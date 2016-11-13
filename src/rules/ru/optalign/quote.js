331d6e85ebf5cbb16e2069a34b7af566698cf8f5
Denis Seleznev <hcodes@yandex.ru>
2016-11-13 15:02:13 +0300

    Поправил расстановку висячей пунктуации

--- src/rules/ru/optalign/quote.js
+++ src/rules/ru/optalign/quote.js
@@ -10,18 +10,17 @@ var classNames = [
 Typograf.rule({
     name: name,
     handler: function(text) {
-        var name = 'ru/punctuation/quote',
+        var quoteName = 'ru/punctuation/quote',
             lquotes = '(["' +
-                this.setting(name, 'lquote') +
-                this.setting(name, 'lquote2') +
-                this.setting(name, 'lquote3') +
+                this.setting(quoteName, 'lquote') +
+                this.setting(quoteName, 'lquote2') +
                 '])',
-            re = new RegExp('([\\d' + this.data('l') + '\\-\u0301!?.:;,]+)( |\u00A0)(' + lquotes + ')', 'gi'),
-            re2 = new RegExp('(^|' + Typograf._privateLabel + ')' + lquotes, 'gm');
+            reNewLine = new RegExp('(^|\n\n|' + Typograf._privateLabel + ')(' + lquotes + ')', 'g'),
+            reInside = new RegExp('([^\n' + Typograf._privateLabel + '])([ \u00A0\n])(' + lquotes + ')', 'gi');
 
         return text
-            .replace(re, '$1<span class="typograf-oa-sp-lquote">$2</span><span class="typograf-oa-lquote">$3</span>')
-            .replace(re2, '$1<span class="typograf-oa-n-lquote">$2</span>');
+            .replace(reNewLine, '$1<span class="typograf-oa-n-lquote">$2</span>')
+            .replace(reInside, '$1<span class="typograf-oa-sp-lquote">$2</span><span class="typograf-oa-lquote">$3</span>');
     },
     disabled: true
 }).innerRule({
--- src/rules/ru/optalign/quote.spec.js
+++ src/rules/ru/optalign/quote.spec.js
@@ -1,14 +1,51 @@
 tests.push(['ru/optalign/quote', [
-    ['Вот у вас «Мой спутник».', 'Вот у вас<span class="typograf-oa-sp-lquote"> </span><span class="typograf-oa-lquote">«</span>Мой спутник».'],
-    ['Вот у вас\n«Мой спутник».', 'Вот у вас\n<span class="typograf-oa-n-lquote">«</span>Мой спутник».'],
-    ['Вот у вас \uDBFF«Мой спутник».\uDBFF', 'Вот у вас \uDBFF<span class="typograf-oa-n-lquote">«</span>Мой спутник».\uDBFF'],
-    ['"что-то", "где-то!"', '<span class="typograf-oa-n-lquote">"</span>что-то",<span class="typograf-oa-sp-lquote"> </span><span class="typograf-oa-lquote">"</span>где-то!"'],
-    ['"что-то, где-то" и "почему-то."', '<span class="typograf-oa-n-lquote">"</span>что-то, где-то" и<span class="typograf-oa-sp-lquote"> </span><span class="typograf-oa-lquote">"</span>почему-то."'],
-    ['тестовый эфир 1 «постно — значит вкусно»', 'тестовый эфир 1<span class="typograf-oa-sp-lquote"> </span><span class="typograf-oa-lquote">«</span>постно — значит вкусно»']
+    [
+        'Вот у вас «Мой спутник».',
+        'Вот у вас<span class="typograf-oa-sp-lquote"> </span><span class="typograf-oa-lquote">«</span>Мой спутник».'
+    ],
+    [
+        'Вот у вас\n«Мой спутник».',
+        'Вот у вас<span class="typograf-oa-sp-lquote">\n</span><span class="typograf-oa-lquote">«</span>Мой спутник».'
+    ],
+    [
+        '«Billboard Hot 100»\n\n«Второе британское вторжение»',
+        '<span class="typograf-oa-n-lquote">«</span>Billboard Hot 100»\n\n<span class="typograf-oa-n-lquote">«</span>Второе британское вторжение»'
+    ],
+    [
+        '«Billboard Hot 100»\n«Второе британское вторжение»\n\n«Billboard Hot 100»\n«Второе британское вторжение»',
+        '<span class="typograf-oa-n-lquote">«</span>Billboard Hot 100»<span class="typograf-oa-sp-lquote">\n</span><span class="typograf-oa-lquote">«</span>Второе британское вторжение»\n\n' +
+            '<span class="typograf-oa-n-lquote">«</span>Billboard Hot 100»<span class="typograf-oa-sp-lquote">\n</span><span class="typograf-oa-lquote">«</span>Второе британское вторжение»'
+    ],
+    [
+        'Вот у вас\n«Мой спутник».\nВот у вас\n«Мой спутник».',
+        'Вот у вас<span class="typograf-oa-sp-lquote">\n</span><span class="typograf-oa-lquote">«</span>Мой спутник».\nВот у вас<span class="typograf-oa-sp-lquote">\n</span><span class="typograf-oa-lquote">«</span>Мой спутник».'
+    ],
+    [
+        'Вот у вас \uDBFF«Мой спутник».\uDBFF',
+        'Вот у вас \uDBFF<span class="typograf-oa-n-lquote">«</span>Мой спутник».\uDBFF'
+    ],
+    [
+        '"что-то", "где-то!"',
+        '<span class="typograf-oa-n-lquote">"</span>что-то",<span class="typograf-oa-sp-lquote"> </span><span class="typograf-oa-lquote">"</span>где-то!"'
+    ],
+    [
+        '"что-то, где-то" и "почему-то."',
+        '<span class="typograf-oa-n-lquote">"</span>что-то, где-то" и<span class="typograf-oa-sp-lquote"> </span><span class="typograf-oa-lquote">"</span>почему-то."'
+    ],
+    [
+        'тестовый эфир 1 «постно — значит вкусно»',
+        'тестовый эфир 1<span class="typograf-oa-sp-lquote"> </span><span class="typograf-oa-lquote">«</span>постно — значит вкусно»'
+    ],
+    [
+        'хранил\n«флаг',
+        'хранил<span class="typograf-oa-sp-lquote">\n</span><span class="typograf-oa-lquote">«</span>флаг',
+    ]
 ]]);
 
 innerTests.push(['ru/optalign/quote', [
+    ['<span class="typograf-oa-sp-lquote">\n</span>', '\n'],
     ['<span class="typograf-oa-sp-lquote"> </span>', ' '],
     ['<span class="typograf-oa-lquote">«</span>', '«'],
+    ['\n<span class="typograf-oa-n-lquote">«</span>', '\n«'],
     ['\n<span class="typograf-oa-n-lquote">«</span>', '\n«']
 ]]);
