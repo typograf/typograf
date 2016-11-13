bd842b94e80c6539624d19a23cfe859e6fbc7010
Denis Seleznev <hcodes@yandex.ru>
2016-11-13 15:02:13 +0300

    Поправил расстановку висячей пунктуации

--- src/rules/ru/optalign/main.js
+++ src/rules/ru/optalign/main.js
@@ -1,10 +1,10 @@
 Typograf._removeOptAlignTags = function(text, classNames) {
-    var re = new RegExp('<span class="(' + classNames.join('|') + ')">(.*?)</span>', 'g');
+    var re = new RegExp('<span class="(' + classNames.join('|') + ')">([^]*?)</span>', 'g');
     return text.replace(re, '$2');
 };
 
 Typograf._removeOptAlignTagsFromTitle = function(text, classNames) {
-    return text.replace(/<title>.*?<\/title>/i, function(text) {
+    return text.replace(/<title>[^]*?<\/title>/i, function(text) {
         return Typograf._removeOptAlignTags(text, classNames);
     });
 };
