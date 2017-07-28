407b67a3c58ac081f7aa4a2663821c43510e9d47
Denis Seleznev <hcodes@yandex.ru>
2017-07-28 03:04:35 +0300

    Доработки в правиле ru/money/currency

--- src/rules/ru/money/currency.js
+++ src/rules/ru/money/currency.js
@@ -2,13 +2,14 @@ Typograf.addRule({
     name: 'ru/money/currency',
     handler: function(text) {
         var currency = '([$€¥Ұ£₤₽])',
-            re1 = new RegExp('(^|[\\D]{2})' + currency + ' ?([\\d.,]+([ \u00A0\u2009\u202F]\\d{3})*)', 'g'),
-            re2 = new RegExp('(^|[\\D])([\\d.,]+) ?' + currency, 'g'),
-            newSubstr1 = '$1$3\u00A0$2',
-            newSubstr2 = '$1$2\u00A0$3';
+            space = '[ \u00A0\u2009\u202F]',
+            re1 = new RegExp('(^|[\\D]{2})' + currency + ' ?([\\d.,]+(' + space + '\\d{3})*)(' + space + '?(тыс\\.|млн|млрд|трлн))?', 'gm'),
+            re2 = new RegExp('(^|[\\D])([\\d.,]+) ?' + currency, 'gm');
 
         return text
-            .replace(re1, newSubstr1)
-            .replace(re2, newSubstr2);
+            .replace(re1, function($0, $1, $2, $3, $4, $5, $6) {
+                return $1 + $3 + ($6 ? '\u00A0' + $6 : '') + '\u00A0' + $2;
+            })
+            .replace(re2, '$1$2\u00A0$3');
     }
 });
