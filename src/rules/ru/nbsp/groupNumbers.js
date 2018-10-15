83d623ea0344ac65fc18f824775de5302603ecbf
Denis Seleznev <hcodes@yandex.ru>
2018-10-15 11:55:22 +0300

    Добавил поддержку тегов для правила ru/nbsp/groupNumber

--- src/rules/ru/nbsp/groupNumbers.js
+++ src/rules/ru/nbsp/groupNumbers.js
@@ -1,7 +1,9 @@
 Typograf.addRule({
     name: 'ru/nbsp/groupNumbers',
     handler(text) {
-        return text.replace(/(^ ?|\D )(\d{1,3}([ \u00A0\u202F\u2009]\d{3})+)(?! ?[\d-])/gm, function($0, $1, $2) {
+        const re = new RegExp(`(^ ?|\\D |${Typograf._privateLabel})(\\d{1,3}([ \u00A0\u202F\u2009]\\d{3})+)(?! ?[\\d-])`, 'gm');
+
+        return text.replace(re, function($0, $1, $2) {
             return $1 + $2.replace(/\s/g, '\u202F');
         });
     }
