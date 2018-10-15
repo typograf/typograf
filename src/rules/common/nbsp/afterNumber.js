d153e75799a7bb5337ee3019fe7826107ab3fffc
Denis Seleznev <hcodes@yandex.ru>
2018-10-15 12:00:33 +0300

    Добавил неразрывный пробел между числом и одной буквой

--- src/rules/common/nbsp/afterNumber.js
+++ src/rules/common/nbsp/afterNumber.js
@@ -3,7 +3,7 @@ Typograf.addRule({
     handler(text, settings, context) {
         const re = '(^|\\D)(\\d{1,5}) ([' +
             context.getData('char') +
-            ']{2,})';
+            ']+)';
 
         return text.replace(new RegExp(re, 'gi'), '$1$2\u00A0$3');
     },
