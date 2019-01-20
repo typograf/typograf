a1609382e54ae607b45e6df78dab536446ef4cd4
Andrey Azov <andrey.azov@gmail.com>
2019-01-20 03:10:31 +0300

    Add en-US rule for em-dashes

---
+++ src/rules/en-US/dash/main.js
@@ -0,0 +1,14 @@
+Typograf.addRule({
+    name: 'en-US/dash/main',
+    index: '-5',
+    handler(text) {
+        const dashes = Typograf.getData('common/dash');
+        const nonBreakingSpace = '\u00A0';
+        const emDash = '\u2014';
+        const spaceBefore = `[ ${nonBreakingSpace}]`; // white space or a non-breaking space
+        const spaceAfter = `[ ${nonBreakingSpace}\n]`; // same as spaceBefore, but includes line break
+        const re = new RegExp(`(${spaceBefore})(${dashes})(${spaceAfter})`, 'g');
+
+        return text.replace(re, `${nonBreakingSpace}${emDash}$3`);
+    }
+});
---
+++ src/rules/en-US/dash/main.spec.js
@@ -0,0 +1,4 @@
+tests.push(['en-US/dash/main', [
+    ['What is serious - and what is not', 'What is serious\u00A0— and what is not'],
+    ['What is serious -- and what is not', 'What is serious\u00A0— and what is not']
+]]);
