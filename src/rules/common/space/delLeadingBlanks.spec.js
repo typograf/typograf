2e210a89ece62ad51b5d00a65280bb55399e5019
Denis Seleznev <hcodes@yandex.ru>
2019-11-02 14:54:02 +0300

    Удалять пробелы в первой строке в common/space/delLeadingBlanks

--- src/rules/common/space/delLeadingBlanks.spec.js
+++ src/rules/common/space/delLeadingBlanks.spec.js
@@ -1,4 +1,10 @@
 tests.push(['common/space/delLeadingBlanks', [
-    ['Hello world!  \t \n \t \t  Hello world!       \n\n\n\n   \t\t\t   Hello world!\n',
-        'Hello world!  \t \nHello world!       \n\n\n\nHello world!\n']
+    [
+        'Hello world!  \t \n \t \t  Hello world!       \n\n\n\n   \t\t\t   Hello world!\n',
+        'Hello world!  \t \nHello world!       \n\n\n\nHello world!\n'
+    ],
+    [
+        '       \t Hello world!  \t \n \t \t  Hello world!\n',
+        'Hello world!  \t \nHello world!\n'
+    ]
 ]]);
