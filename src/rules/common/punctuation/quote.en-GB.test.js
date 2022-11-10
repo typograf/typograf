003ea419df1c83d527c05736cf378ad472ef9c15
Aleksandr Yakunichev <hi@ya.codes>
2022-11-10 15:54:01 +0100

    Add en-GB quotes to common/quotes list

--- src/rules/common/punctuation/quote.en-GB.test.js
+++ src/rules/common/punctuation/quote.en-GB.test.js
@@ -17,3 +17,24 @@ typografRuleTest([
     ],
     {locale: 'en-GB'}
 ]);
+
+/**
+ * Check if en-GB single quotes can be changed to en-US ones.
+ */ 
+typografRuleTest([
+    'common/punctuation/quote', [
+        [
+            'One of the most famous phrases is ‘to be or not to be’.',
+            'One of the most famous phrases is “to be or not to be”.'
+        ],
+        [
+            '‘I have no special talent,’ Einstein. ‘I am only curious enough.’',
+            '“I have no special talent,” Einstein. “I am only curious enough.”'
+        ],
+        [
+            '‘I was reading “The Economics of the USA” yesterday,’ she replied to me.',
+            '“I was reading ‘The Economics of the USA’ yesterday,” she replied to me.'
+        ]
+    ],
+    {locale: 'en-US'}
+]);
