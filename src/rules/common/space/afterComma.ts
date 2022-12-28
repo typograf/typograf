2f895baf20ac78f3910c2ae925f4f0e2d1119526
Aleksandr Yakunichev <hi@ya.codes>
2022-12-28 16:55:52 +0100

    Fix afterComma before quotes

--- src/rules/common/space/afterComma.ts
+++ src/rules/common/space/afterComma.ts
@@ -1,14 +1,15 @@
+import { DataQuote, DataCommonQuote } from '../../../data';
 import type { TypografRule } from '../../../main';
 import { privateLabel } from '../../../consts';
 import { isDigit } from '../../../helpers/regExp';
 
-const reComma = new RegExp('(.),([^)",:.?\\s\\/\\\\' + privateLabel + '])', 'g');
-
 export const afterCommaRule: TypografRule = {
     name: 'common/space/afterComma',
-    handler(text) {
+    handler(text, settings, context) {
+        const quote = context.getData('quote');
+        const quotes = typeof quote === 'string' ? quote as DataCommonQuote : (quote as DataQuote).right;
         return text.replace(
-            reComma,
+            new RegExp('(.),([^)",:.?\\s\\/\\\\' + privateLabel + quotes + '])', 'g'),
             ($0, $1, $2) => isDigit($1) && isDigit($2) ? $0 : $1 + ', ' + $2
         );
     }
