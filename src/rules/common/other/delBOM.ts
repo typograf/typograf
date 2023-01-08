218bc585f2d961736b27b07a0a203231073ec183
Denis Seleznev <hcodes@users.noreply.github.com>
2023-01-08 22:49:09 +0300

    Fix index

--- src/rules/common/other/delBOM.ts
+++ src/rules/common/other/delBOM.ts
@@ -2,7 +2,7 @@ import type { TypografRule } from '../../../main';
 export const delBOMRule: TypografRule = {
     name: 'common/other/delBOM',
     queue: 'start',
-    index: '-1',
+    index: -1,
     handler(text) {
         if (text.charCodeAt(0) === 0xFEFF) {
             return text.slice(1);
