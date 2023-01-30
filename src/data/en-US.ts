67cdfc980f2641cc8fedbd40e0f4bb5be873c3fc
Konstantin Sipunin <k.sipunin@just-ai.com>
2023-01-30 22:55:40 +0300

    Update word lists

--- src/data/en-US.ts
+++ src/data/en-US.ts
@@ -4,5 +4,5 @@ export default {
         left: '“‘',
         right: '”’',
     },
-    'en-US/shortWord': 'a|an|and|as|at|but|by|in|nor|not|on|or|out|so|the|to|yet',
+    'en-US/shortWord': 'a|an|and|as|at|bar|but|by|for|if|in|nor|not|of|off|on|or|out|per|pro|so|the|to|up|via|yet',
 };
