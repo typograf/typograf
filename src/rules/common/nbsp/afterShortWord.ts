218891d27a32cf1cccf5efbfbcca1d388577e0c4
Konstantin Sipunin <k.sipunin@just-ai.com>
2023-09-18 21:36:50 +0600

    Fix short word list possibly being undefined

--- src/rules/common/nbsp/afterShortWord.ts
+++ src/rules/common/nbsp/afterShortWord.ts
@@ -8,19 +8,14 @@ export const afterShortWordRule: TypografRule<{
 }> = {
     name: 'common/nbsp/afterShortWord',
     handler(text, settings, context) {
-        const {lengthShortWord: len, useShortWordList} = settings;
+        const { lengthShortWord, useShortWordList } = settings;
         const quote = getData('common/quote') as DataCommonQuote;
+        const char = context.getData('char') as DataChar;
+        const shortWord = context.getData('shortWord') as DataChar | undefined;
         const before = ' \u00A0(' + privateLabel + quote;
-        let subStr;
-
-        if (useShortWordList) {
-            const shortWords = context.getData('shortWord');
-            subStr = '(^|[' + before + '])(' + shortWords + ') ';
-        } else {
-            const char = context.getData('char') as DataChar;
-            subStr = '(^|[' + before + '])([' + char + ']{1,' + len + '}) ';
-        }
-
+        const subStr = useShortWordList && shortWord !== undefined
+            ? '(^|[' + before + '])(' + shortWord + ') '
+            : '(^|[' + before + '])([' + char + ']{1,' + lengthShortWord + '}) ';
         const newSubStr = '$1$2\u00A0';
         const re = new RegExp(subStr, 'gim');
 
