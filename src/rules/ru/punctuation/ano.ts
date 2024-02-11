0fa96753ae136c88f40ffdafd82e8745347247f0
denis.bosyi <denis.bosiy@gmail.com>
2024-02-11 23:24:16 +0300

    fixed 400 issue

--- src/rules/ru/punctuation/ano.ts
+++ src/rules/ru/punctuation/ano.ts
@@ -4,8 +4,15 @@ import { privateLabel } from '../../../consts';
 export const anoRule: TypografRule = {
     name: 'ru/punctuation/ano',
     handler(text) {
-        const re = new RegExp('([^«„[(!?,:;\\-‒–—\\s' + privateLabel + '])(\\s+)(а|но)(?= |\u00A0|\\n)', 'g');
+        const parts: string[] = [
+            '([^«„[(!?,:;\\-‒–—\\s' + privateLabel + '])', // Запятую не ставим, если уже есть какой-либо знак
+            '(?<!([^а-яА-Я]+ну|Ну))', // Запятую не ставим перед ну
+            '(\\s+)', // Отступ между левой и правой частями в виде пробельных символов
+            '(а|но)', // Союзы
+            '(?= |\u00A0|\\n)' // Остальная часть предложения(пробел, неразрывной пробел, перевод строки)
+        ]
+        const re = new RegExp(parts.join(''), 'g');
 
-        return text.replace(re, '$1,$2$3');
+        return text.replace(re, '$1,$3$4');
     },
 };
