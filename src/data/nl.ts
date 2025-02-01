e9395be99b476283e384f25b691bc364b5ed993d
George Bougakov <me@gbgk.me>
2025-02-01 02:56:09 +0100

    Dutch short words

--- src/data/nl.ts
+++ src/data/nl.ts
@@ -1,7 +1,8 @@
 export default {
     'nl/char': 'a-zäçèéêëîïñöûü',
     'nl/quote': {
-        left: '‘“',
-        right: '’”',
-    }
+        left: '„‚',
+        right: '”’',
+    },
+    'nl/shortWord': "a|aan|al|als|ben|bij|dan|dag|dat|de|die|dit|dus|een|en|er|ge|gij|haar|hen|het|hij|hun|ik|in|je|jij|jou|me|met|mij|m'n|n|of|om|ons|op|per|'t|te|tot|u|uit|uw|van|via|wat|we|wel|wie|wij|zal|ze|zij|zijn|zo|'s|des",
 };
